876c93faf6d7c808f2c38344742e0faabba237f6
AAZANKO <aleksandr.zan87@gmail.com>
2019-03-01 17:40:13 +0300

    one to commit

---
+++ src/main/java/by/htp/zanko/ArraySorting.java
@@ -0,0 +1,34 @@
+package by.htp.zanko;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class ArraySorting {
+
+    public int[] getArraySorting(int[] arrayInt){
+
+        for (int i = 0; i < arrayInt.length; i++) {
+
+            int min = arrayInt[i];
+            int min_i = i;
+
+            if ((i % 2) == 0) {
+                for (int j = i + 2; j < arrayInt.length; j++) {
+
+                    if (arrayInt[j] < min) {
+                        min = arrayInt[j];
+                        min_i = j;
+                    }
+                }
+
+                if (i != min_i) {
+                    int tmp = arrayInt[i];
+                    arrayInt[i] = arrayInt[min_i];
+                    arrayInt[min_i] = tmp;
+                }
+            }
+        }
+
+        return arrayInt;
+    }
+}
---
+++ src/test/java/by/htp/zanko/ArraySortingTest.java
@@ -0,0 +1,21 @@
+package by.htp.zanko;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ArraySortingTest {
+
+    @Test
+    public void checkGetArraySorting(){
+
+        int[] arrayInt = {5, 3, 2, 8, 1, 4, 9, 7};
+
+        ArraySorting arraySorting = new ArraySorting();
+
+        int[] arraySortingResult = arraySorting.getArraySorting(arrayInt);
+
+        Assert.assertNotNull(arraySortingResult);
+    }
+
+
+}
